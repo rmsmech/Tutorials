b64e513388f0f70e6114f616f74c1834df41c18f
Mahalingam S <senguttuvan.mahalingam@simdev.ch>
2022-04-11 19:18:38 +0400

    Quick MVVM setup

---
+++ QuickMVVMSetup/QuickMVVMSetup/MainVM.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+using Haley.Models;
+using Haley.Abstractions;
+using Haley.MVVM;
+
+namespace QuickMVVMSetup
+{
+    public class MainVM : ChangeNotifier
+    {
+        private Person person;
+        public Person TargetPerson
+        {
+            get { return person; }
+            set { person = value; OnPropertyChanged(nameof(TargetPerson)); }
+        }
+
+        private ObservableCollection<Person> _persons; //ONLY WHEN ADDED/OR REMOVED (NOT FOR INTERNAL PROPERTY CHANGES)
+        public ObservableCollection<Person> Persons
+        {
+            get { return _persons; }
+            set { _persons = value; }
+        }
+
+        public void AddPerson()
+        {
+            Persons.Add(TargetPerson); //Add it to thecollection
+            TargetPerson = new Person(); //resetting it.
+        }
+
+        public ICommand CMDAdd => new DelegateCommand(AddPerson);
+        public ICommand CMDDelete => new DelegateCommand<Person>(DeletePerson);
+
+        private void DeletePerson(Person obj)
+        {
+            if (obj == null) return;
+            if (!Persons.Contains(obj)) return;
+
+            Persons.Remove(obj);
+        }
+
+        public MainVM()
+        {
+            Persons = new ObservableCollection<Person>();
+            TargetPerson = new Person();
+        }
+    }
+}
---
+++ QuickMVVMSetup/QuickMVVMSetup/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using Haley.Models;
+using Haley.Abstractions;
+using Haley.MVVM;
+
+namespace QuickMVVMSetup
+{
+    public class Person : ChangeNotifier
+    {
+        private string _FName;
+        public string FName
+        {
+            get { return _FName; }
+            set { _FName = value; OnPropertyChanged(nameof(FName)); }
+        }
+
+        private string _LName;
+        public string LName
+        {
+            get { return _LName; }
+            set { _LName = value; OnPropertyChanged(nameof(LName)); }
+        }
+
+        private string _Age;
+        public string Age
+        {
+            get { return _Age; }
+            set { _Age = value; OnPropertyChanged(nameof(Age)); }
+        }
+
+        public Person() { }
+    }
+}
