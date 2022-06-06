c7780c08d267c27d9f19cf9d15ba2d3fa58bda03
Mahalingam S <senguttuvan.mahalingam@simdev.ch>
2022-06-06 13:00:05 +0400

    added round button project

---
+++ RoundButtonDemo/RoundButtonDemo/RoundButton.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RoundButtonDemo
+{
+    /// <summary>
+    /// Interaction logic for RoundButton.xaml
+    /// </summary>
+    public partial class RoundButton : UserControl
+    {
+        private static CornerRadius _defaultCornerRadius = new CornerRadius(10.0);
+
+        public RoundButton()
+        {
+            InitializeComponent();
+        }
+
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register(nameof(CornerRadius), typeof(CornerRadius), typeof(RoundButton), new PropertyMetadata(_defaultCornerRadius));
+
+        public new object Content
+        {
+            get { return (object)GetValue(ContentProperty); }
+            set { SetValue(ContentProperty, value); }
+        }
+
+        public static new readonly DependencyProperty ContentProperty =
+            DependencyProperty.Register(nameof(Content), typeof(object), typeof(RoundButton), new PropertyMetadata(null));
+
+        public new Brush Background
+        {
+            get { return (Brush)GetValue(BackgroundProperty); }
+            set { SetValue(BackgroundProperty, value); }
+        }
+
+        public static new readonly DependencyProperty BackgroundProperty =
+            DependencyProperty.Register("Background", typeof(Brush), typeof(RoundButton), new PropertyMetadata(Brushes.Gray));
+
+        public new Thickness BorderThickness
+        {
+            get { return (Thickness)GetValue(BorderThicknessProperty); }
+            set { SetValue(BorderThicknessProperty, value); }
+        }
+
+        public static new readonly DependencyProperty BorderThicknessProperty =
+            DependencyProperty.Register("BorderThickness", typeof(Thickness), typeof(RoundButton), new PropertyMetadata(new Thickness(0.0)));
+
+        public ICommand Command
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register("Command", typeof(ICommand), typeof(RoundButton), new PropertyMetadata(null));
+
+        public object CommandParameter
+        {
+            get { return (object)GetValue(CommandParameterProperty); }
+            set { SetValue(CommandParameterProperty, value); }
+        }
+
+        public static readonly DependencyProperty CommandParameterProperty =
+            DependencyProperty.Register("CommandParameter", typeof(object), typeof(RoundButton), new PropertyMetadata(null));
+
+        public event RoutedEventHandler Click;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Click?.Invoke(sender, e);
+            //reasone why we have ? after click is that there is a possiblility that no one is subscribing to this.
+
+            //If you need, you can handle it here or make it a bubble event.
+            e.Handled = false; //If you set true, then no other event will be raised (in case of a bubble or tunnel event)
+        }
+    }
+}
---
+++ RoundButtonDemo/RoundButtonDemo/Themes/SimpleRoundButton.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RoundButtonDemo.Themes
+{
+    public class SimpleRoundButton : Button
+    {
+        private static CornerRadius _defaultCornerRadius = new CornerRadius(10.0);
+        static SimpleRoundButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SimpleRoundButton), new FrameworkPropertyMetadata(typeof(SimpleRoundButton)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+        }
+
+        //Since we are overriding a button, we already have all the properties that we need. We will merely over write the UI in a resource dictionary and we will be done.
+        // We don't need to do anything els other than the new properties that we will add
+
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register("CornerRadius", typeof(CornerRadius), typeof(SimpleRoundButton), new PropertyMetadata(_defaultCornerRadius));
+    }
+}
