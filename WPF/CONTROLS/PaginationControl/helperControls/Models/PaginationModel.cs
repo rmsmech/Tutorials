21ee9042d805ac46135e4241081a974cfbfc6708
Senguttuvan Mahalingam <rmsmech@gmail.com>
2022-12-05 18:19:05 +0400

    moved pagination

---
+++ WPF/CONTROLS/PaginationControl/helperControls/Models/PaginationModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using Haley.Models;
+using Haley.MVVM;
+using Haley.Abstractions;
+using Haley.Events;
+using Haley.Enums;
+using Haley.Utils;
+
+
+namespace helperControls.Models
+{
+    public class PaginationModel : ChangeNotifier
+    {
+        public int maxitemsperpage { get; set; }
+
+        private int _total_items;
+        public int total_items
+        {
+            get { return _total_items; }
+            set { _total_items = value; onPropertyChanged(); }
+        }
+
+        private int _items_per_page;
+        public int items_per_page
+        {
+            get { return _items_per_page; }
+            set { _items_per_page = value;onPropertyChanged(); }
+        }
+
+        private int _current_page;
+        public int current_page
+        {
+            get { return _current_page; }
+            set 
+            { 
+                if (value < total_pages+1 && value > 0)
+                {
+                    _current_page = value;
+                    onPropertyChanged(); 
+                }
+            }
+        }
+
+        private int _total_pages;
+        public int total_pages
+        {
+            get { return _total_pages; }
+            set { _total_pages = value;onPropertyChanged(); }
+        }
+
+        private void _initiation(int _totalitemscount, int _pageitemcount)
+        {
+            total_items = _totalitemscount;
+            items_per_page = _pageitemcount;
+
+            int remainder_check = 0;
+            Math.DivRem(total_items, items_per_page, out remainder_check);
+            total_pages = total_items / items_per_page;
+            if (remainder_check != 0) total_pages++; //Increment by 1
+
+            current_page = 1;
+            maxitemsperpage = 15;
+        }
+
+        public PaginationModel(int _totalitemscount, int _pageitemcount) 
+        {
+            _initiation(_totalitemscount, _pageitemcount);
+        }
+
+    }
+}
---
+++ WPF/CONTROLS/PaginationControl/helperControls/ViewModel/VMPagination.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Haley.Abstractions;
+using Haley.Enums;
+using Haley.Events;
+using Haley.Models;
+using Haley.MVVM;
+using Haley.Utils;
+using helperControls.Models;
+
+
+
+namespace helperControls.ViewModel
+{
+    public class VMPagination : ChangeNotifier
+    {
+        #region Properties
+        private PaginationModel _pagination;
+        public PaginationModel pagination
+        {
+            get { return _pagination; }
+            set { _pagination = value; onPropertyChanged(); }
+        }
+
+        #endregion
+
+        #region Command Properties
+        public ICommand cmd_change_page { get; set; }
+
+        #endregion
+
+        #region CommandMethods
+        private void changePage(string obj)
+        {
+            try
+            {
+                int parameter = int.Parse(obj);
+                int newpage = _pagination.current_page; //Get current page
+
+                switch (parameter)
+                {
+                    case 0:
+                        newpage--;
+                        if (newpage < 1) newpage = 1;
+                        break;
+                    case 1:
+                        newpage++;
+                        if (newpage > _pagination.total_pages) newpage = _pagination.total_pages;
+                        break;
+                }
+                _pagination.current_page = newpage;
+               
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void calculatePagination()
+        {
+            try
+            {
+                if (pagination.items_per_page == 0) pagination.items_per_page = 1; //Setting minimum
+                if (pagination.items_per_page > pagination.total_items) pagination.items_per_page = pagination.total_items; //Setting maximum
+                pagination.total_pages = pagination.total_items / pagination.items_per_page; // Problem to be solved
+                pagination.current_page = 1;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        #endregion
+
+        #region Methods
+
+        private void _initiation()
+        {
+            cmd_change_page = new DelegateCommand<string>(changePage, null);
+        }
+
+        public void seed(PaginationModel pagination)
+        {
+            _pagination = pagination;
+        }
+
+        #endregion
+
+        public VMPagination() 
+        { 
+            _initiation(); 
+
+        }
+    }
+}
