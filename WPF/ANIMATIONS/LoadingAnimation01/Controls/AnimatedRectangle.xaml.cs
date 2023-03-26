4776ec84265d90c0c5da857e54c44aab4aa1353a
Senguttuvan Mahalingam <rmsmech@gmail.com>
2023-03-26 23:48:08 +0400

    WPF Loading Animation - Rotating Rectangle

---
+++ WPF/ANIMATIONS/LoadingAnimation01/Controls/AnimatedRectangle.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoadingAnimation01.Controls {
+    /// <summary>
+    /// Interaction logic for AnimatedRectangle.xaml
+    /// </summary>
+    public partial class AnimatedRectangle : UserControl {
+        public AnimatedRectangle() {
+            InitializeComponent();
+        }
+
+        public double? To {
+            get { return (double?)GetValue(ToProperty); }
+            set { SetValue(ToProperty, value); }
+        }
+        public static readonly DependencyProperty ToProperty =
+            DependencyProperty.Register("To", typeof(double?), typeof(AnimatedRectangle), new PropertyMetadata(360.0));
+
+        public string Message {
+            get { return (string)GetValue(MessageProperty); }
+            set { SetValue(MessageProperty, value); }
+        }
+
+        public static readonly DependencyProperty MessageProperty =
+            DependencyProperty.Register("Message", typeof(string), typeof(AnimatedRectangle), new PropertyMetadata(string.Empty));
+
+        public Brush Stroke {
+            get { return (Brush)GetValue(StrokeProperty); }
+            set { SetValue(StrokeProperty, value); }
+        }
+
+        public static readonly DependencyProperty StrokeProperty =
+            DependencyProperty.Register("Stroke", typeof(Brush), typeof(AnimatedRectangle), new PropertyMetadata(Brushes.Gray));
+
+        public Duration Duration {
+            get { return (Duration)GetValue(DurationProperty); }
+            set { SetValue(DurationProperty, value); }
+        }
+
+        public static readonly DependencyProperty DurationProperty =
+            DependencyProperty.Register("Duration", typeof(Duration), typeof(AnimatedRectangle), new PropertyMetadata(default(Duration)));
+    }
+}
